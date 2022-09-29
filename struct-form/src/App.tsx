6645a1002efeb82dcaed7c4ad0a66650bedb6081
[Gregorie Reyes] <[reyesgregorie@gmail.com]>
2022-09-30 00:54:13 +0200

    simple form

---
+++ struct-form/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SignUp from "./SignUp";
+
+const App = () => {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <SignUp />
+    </div>
+  );
+};
+
+export default App;
